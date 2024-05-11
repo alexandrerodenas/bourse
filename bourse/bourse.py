98b3f0650cb5d2a7da881bc78828f54787443848
Alexandre Rodenas <alexandre.rodenas-prestataire@laposte.fr>
2024-05-11 20:53:27 +0200

    Add table

--- bourse/bourse.py
+++ bourse/bourse.py
@@ -1,22 +1,48 @@
 from typing import List
 
 import reflex as rx
+from reflex.components.radix.themes.components.table import TableRow, TableCell
 
 from bourse.portfolio import Portfolio
-from bourse.stock import load_stocks_from_yaml
+from bourse.stock import load_stocks_from_yaml, Stock
 
 portfolio = Portfolio(stocks=load_stocks_from_yaml("stocks.yml"))
 
+MIN_GAIN = -20
+MAX_GAIN = 20
 
-class State(rx.State):
-    data: List = [stock.transform_to_dict() for stock in portfolio.stocks]
-    columns: List[str] = ["symbol"]
+
+def get_color_style(stock: Stock) -> str:
+    normalized_value = (stock.gain_or_deficit - MIN_GAIN) / (MAX_GAIN - MIN_GAIN)
+    g = int(255 * normalized_value)
+    r = int(255 * (1 - normalized_value))
+    return f"rgb({r}, {g}, 0)"
 
 
 def index():
-    return rx.data_table(
-        data=State.data,
-        columns=State.columns,
+    headers = rx.table.header(
+        rx.table.row(
+            rx.table.column_header_cell("Name"),
+            rx.table.column_header_cell("Date"),
+            rx.table.column_header_cell("Cost"),
+            rx.table.column_header_cell("Estimation"),
+            rx.table.column_header_cell("+/-"),
+            rx.table.column_header_cell("Status"),
+        ),
+    )
+    rows = [rx.table.row(
+        rx.table.cell(stock.name),
+        rx.table.cell(stock.date),
+        rx.table.cell(stock.cost),
+        rx.table.cell(stock.estimation),
+        rx.table.cell(f"{stock.difference_value_euros}€ ({round(stock.difference_value_percentage, 2)}%)"),
+        rx.table.cell(stock.status),
+        background=get_color_style(stock)
+    ) for stock in portfolio.stocks]
+    body = rx.table.body(*rows)
+    return rx.table.root(
+        headers,
+        body,
     )
 
 
--- bourse/stock.py
+++ bourse/stock.py
@@ -3,7 +3,8 @@ import yfinance as yf
 
 
 class Stock:
-    def __init__(self, symbol, number, unit_cost, date, status):
+    def __init__(self, name, symbol, number, unit_cost, date, status):
+        self.name = name
         self.symbol = symbol
         self.number = number
         self.date = date
@@ -11,7 +12,7 @@ class Stock:
         self.cost = unit_cost * number
         self.current_price = self.get_current_price()
         self.gain_or_deficit = self.calculate_gain_or_deficit()
-        self.stock_value_estimation = self.calculate_stock_value_estimation()
+        self.estimation = self.calculate_stock_value_estimation()
         self.difference_value_euros = self.calculate_difference_value_euros()
         self.difference_value_percentage = self.calculate_difference_value_percentage()
 
@@ -29,13 +30,14 @@ class Stock:
         return self.current_price * self.number
 
     def calculate_difference_value_euros(self):
-        return round(self.stock_value_estimation - self.cost, 2)
+        return round(self.estimation - self.cost, 2)
 
     def calculate_difference_value_percentage(self):
         return (self.difference_value_euros / self.cost) * 100 if self.cost != 0 else 0
 
     def transform_to_dict(self):
         return {
+            'name': self.name,
             'symbol': self.symbol,
             'number': self.number,
             'cost': self.cost,
@@ -43,13 +45,13 @@ class Stock:
             'status': self.status,
             'current_price': self.current_price,
             'gain_or_deficit': self.gain_or_deficit,
-            'stock_value_estimation': self.stock_value_estimation,
+            'stock_value_estimation': self.estimation,
             'difference_value_euros': self.difference_value_euros,
             'difference_value_percentage': self.difference_value_percentage
         }
 
     def to_string(self):
-        return f"Symbol: {self.symbol}\nNumber of Shares: {self.number}\nCost: {self.cost}\nDate: {self.date}\nStatus: {self.status}\nCurrent Price: {self.current_price}\nGain or Deficit: {self.gain_or_deficit}\nStock Value Estimation: {self.stock_value_estimation}\nDifference Value (Euros): {self.difference_value_euros}\nDifference Value (Percentage): {self.difference_value_percentage:.2f}%\n"
+        return f"Name: {self.name}\nSymbol: {self.symbol}\nNumber of Shares: {self.number}\nCost: {self.cost}\nDate: {self.date}\nStatus: {self.status}\nCurrent Price: {self.current_price}\nGain or Deficit: {self.gain_or_deficit}\nStock Value Estimation: {self.estimation}\nDifference Value (Euros): {self.difference_value_euros}\nDifference Value (Percentage): {self.difference_value_percentage:.2f}%\n"
 
 
 def load_stocks_from_yaml(file_path):
@@ -61,6 +63,7 @@ def load_stocks_from_yaml(file_path):
 
     for stock_name, stock_data in data.items():
         stock = Stock(
+            name=stock_name,
             symbol=stock_data["symbol"],
             number=stock_data["number"],
             unit_cost=stock_data["cost"],
