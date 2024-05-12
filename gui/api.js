8d8427705c157a02b7b0ed933c085f6928b8fce4
Alexandre Rodenas <alexandre.rodenas-prestataire@laposte.fr>
2024-05-12 19:43:58 +0200

    Extract api in dedicated file web server

---
+++ gui/api.js
@@ -0,0 +1,4 @@
+export function fetchData(url) {
+  return fetch(url)
+  .then(response => response.json());
+}
--- gui/script.js
+++ gui/script.js
@@ -1,4 +1,6 @@
-const API_URL = 'http://localhost:5000/history';
+import { fetchData } from './api.js';
+
+const API_URL = 'http://localhost:8000/history';
 const DATE_FORMAT_OPTIONS = {
     day: '2-digit',
     month: '2-digit',
@@ -29,15 +31,12 @@ document.addEventListener('DOMContentLoaded', function () {
         .catch(error => console.error('Error fetching data:', error));
 });
 
-function fetchData(url) {
-    return fetch(url)
-        .then(response => response.json());
-}
 
-function createTabAndDataTable(entry, index) {
-    var formattedDate = formatDate(entry.portfolio_date);
-    var tab = createTab(formattedDate);
-    var tabPane = createTabPane(formattedDate);
+
+function createTabAndDataTable(entry, _) {
+    const formattedDate = formatDate(entry.portfolio_date);
+    createTab(formattedDate);
+    const tabPane = createTabPane(formattedDate);
     populateDataTable(tabPane.id, entry.stocks);
     createInfoSeparator(tabPane.id);
     populateAdditionalInfo(entry, tabPane.id);
@@ -66,64 +65,66 @@ function createTabPane(formattedDate) {
 }
 
 function populateDataTable(tabPaneId, stocks) {
-    var tableContainer = document.createElement('div');
+    const tableContainer = document.createElement('div');
     tableContainer.classList.add('table-container');
     document.getElementById(tabPaneId).appendChild(tableContainer);
 
-    var table = document.createElement('table');
+    const table = document.createElement('table');
     table.classList.add('table', 'is-bordered', 'is-striped', 'is-narrow', 'is-hoverable', 'is-fullwidth');
     table.id = `table${tabPaneId.substr(3)}`;
     tableContainer.appendChild(table);
-
-    var dataTable = new DataTable(table, {
+    new DataTable(table, {
         data: stocks,
         columns: TABLE_OPTIONS,
         lengthMenu: LENGTH_MENU_OPTIONS,
         pageLength: DEFAULT_PAGE_LENGTH,
         createdRow: createRow,
         columnDefs: [
-            { targets: [0, 1], className: 'has-text-centered' }
+            { targets: [0, 1], className: 'has-text-centered' },
         ],
         autoWidth: false,
     });
-
     tableContainer.style.overflowY = 'auto';
     tableContainer.style.overflowX = 'hidden';
 }
 
 function createRow(row, data) {
-    var currentPriceCell = row.getElementsByTagName('td')[3];
-    var currentPrice = parseFloat(currentPriceCell.innerHTML);
+    const currentPriceCell = row.getElementsByTagName('td')[3];
+    const currentPrice = parseFloat(currentPriceCell.innerHTML);
     currentPriceCell.innerHTML = currentPrice.toFixed(2);
 
-    var gainLossCell = row.getElementsByTagName('td')[4];
-    var gainLoss = parseFloat(gainLossCell.innerHTML);
+    const gainLossCell = row.getElementsByTagName('td')[4];
+    const gainLoss = parseFloat(gainLossCell.innerHTML);
     if (gainLoss < 0) {
         row.style.backgroundColor = RED_COLOR;
     }
+
+    const nomCell = row.getElementsByTagName('td')[0];
+    nomCell.innerHTML = capitalizeString(nomCell.innerHTML)
+}
+
+function capitalizeString(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
 function activateFirstTab() {
-    var firstTab = document.querySelector('#apiTabs ul li:first-child a');
-    firstTab.classList.add('is-active');
-    var firstTabPaneId = firstTab.getAttribute('href').substring(1);
-    document.getElementById(firstTabPaneId).classList.add('is-active');
+    const firstTabLink = document.querySelector('#apiTabs ul li:first-child a');
+    firstTabLink.click()
 }
 
 function attachTabClickListeners() {
-    var tabLinks = document.querySelectorAll('#apiTabs ul li a');
+    const tabLinks = document.querySelectorAll('#apiTabs ul li a');
     tabLinks.forEach(function(tabLink) {
         tabLink.addEventListener('click', function(event) {
-            var targetId = this.getAttribute('href').substring(1);
-            var targetPane = document.getElementById(targetId);
+            const targetId = this.getAttribute('href').substring(1);
+            const targetPane = document.getElementById(targetId);
             document.querySelectorAll('.tab-pane').forEach(function(tabPane) {
                 tabPane.classList.remove('is-active');
                 tabPane.style.display = 'none';
             });
-            document.querySelectorAll('#apiTabs ul li a').forEach(function(tab) {
+            document.querySelectorAll('#apiTabs ul li').forEach(function(tab) {
                 tab.classList.remove('is-active');
             });
-            this.classList.add('is-active');
             targetPane.classList.add('is-active');
             targetPane.style.display = 'block'
         });
@@ -131,17 +132,18 @@ function attachTabClickListeners() {
 }
 
 function createInfoSeparator(tabPaneId) {
-    var separator = document.createElement('hr');
+    const separator = document.createElement('hr');
     separator.classList.add('info-separator');
     document.getElementById(tabPaneId).appendChild(separator);
 }
 
 function populateAdditionalInfo(entry, tabPaneId) {
-    var infoContainer = document.createElement('div');
+    const infoContainer = document.createElement('div');
     infoContainer.classList.add('info-container');
-    var totalGainDeficit = parseFloat(entry.total_gain_deficit).toFixed(2);
-    var totalInvestmentAmount = parseFloat(entry.total_investment_amount).toFixed(2);
-    var totalMarketValue = parseFloat(entry.total_market_value).toFixed(2);
+    const totalGainDeficit = parseFloat(entry.total_gain_deficit).toFixed(2);
+    const totalInvestmentAmount = parseFloat(entry.total_investment_amount).
+      toFixed(2);
+    const totalMarketValue = parseFloat(entry.total_market_value).toFixed(2);
     infoContainer.innerHTML = `
         <h3><i class="fas fa-info-circle"></i> Portfolio information</h3>
         <p><i class="fas fa-money-bill"></i> Montant investit: ${totalInvestmentAmount}€</p>
